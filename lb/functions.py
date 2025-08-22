b285c3efc75ffedfd705cb6e31167ab2b348b0be
Elijah1123 <elijahmzalendo659@gmail.com>
2025-08-22 14:17:00 +0300

    python

---
+++ lb/functions.py
@@ -0,0 +1,11 @@
+def greet_programmer():
+    print(f"Hello, Programmer!")
+def greet(name):
+    print(f"Hello, {name}!") 
+def greet_with_default(name="Programmer"):
+    print(f"Hello, {name}!")    
+def add(num1, num2):
+    return num1 + num2
+def halve(number):
+    return number / 2
+
